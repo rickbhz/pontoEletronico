34cc576e15210c6a94625c5180363a780e6194be
Carlos Salgado <carlos.d.salgado@gmail.com>
2019-01-03 17:46:31 -0200

    Commit Inicial

---
+++ src/main/java/com/cadu/pontoEletronico/api/PontoEletronicoApplication.java
@@ -0,0 +1,14 @@
+package com.cadu.pontoEletronico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoEletronicoApplication.class, args);
+	}
+
+}
+
